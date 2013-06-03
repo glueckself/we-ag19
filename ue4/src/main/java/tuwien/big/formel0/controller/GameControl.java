1e9c02cb76cb952c05cdde42ae22437f4ca0cb60
Julian Grosshauser <hi@juliangrosshauser.com>
2013-06-03 17:42:06 +0200

    implemented uuid

--- ue4/src/main/java/tuwien/big/formel0/controller/GameControl.java
+++ ue4/src/main/java/tuwien/big/formel0/controller/GameControl.java
@@ -7,6 +7,15 @@ import formel0api.Player;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import tuwien.big.formel0.twitter.*;
+import at.ac.tuwien.big.we.highscore.Failure;
+import at.ac.tuwien.big.we.highscore.PublishHighScoreService;
+import at.ac.tuwien.big.we.highscore.PublishHighScoreEndpoint;
+import at.ac.tuwien.big.we.highscore.data.HighScoreRequestType;
+import at.ac.tuwien.dbai.education.ssd.ss13.uebung.tournament.*;
+import java.util.GregorianCalendar;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import java.math.BigInteger;
 
 @ManagedBean(name = "gc")
 @SessionScoped
@@ -113,8 +122,83 @@ public class GameControl {
             // this part is (and must be) run only once per game (when the game is won)
 
             // TODO: Get UUID from the leaderboard
+            PublishHighScoreService highScoreService = new PublishHighScoreService();
+            PublishHighScoreEndpoint highScore = highScoreService.getPublishHighScorePort();
+            
+            at.ac.tuwien.big.we.highscore.data.ObjectFactory factory = new at.ac.tuwien.big.we.highscore.data.ObjectFactory();
+            HighScoreRequestType highScoreRequest = factory.createHighScoreRequestType();
+            highScoreRequest.setUserKey("34EphAp2C4ebaswu");
+            
+            ObjectFactory objectFactory = new ObjectFactory();
+            TournamentType tournament = objectFactory.createTournamentType();
+            
+            GregorianCalendar currentTime = new GregorianCalendar();
+            try {
+                XMLGregorianCalendar XMLCurrentTime = DatatypeFactory.newInstance().newXMLGregorianCalendar(currentTime);
+                tournament.setStartDate(XMLCurrentTime);
+                tournament.setEndDate(XMLCurrentTime);
+                tournament.setRegistrationDeadline(XMLCurrentTime);
+            } catch(Exception e) {
+                System.err.println(e.getMessage());
+            }
+            
+            TournamentType.Players players = objectFactory.createTournamentTypePlayers();
+            TournamentType.Players.Player currentPlayer = objectFactory.createTournamentTypePlayersPlayer();
+            
+            //TODO: get real date of birth
+            GregorianCalendar calendar = new GregorianCalendar(1990, 12, 31);
+            try {
+                XMLGregorianCalendar dateOfBirth = DatatypeFactory.newInstance().newXMLGregorianCalendar(calendar);
+                currentPlayer.setDateOfBirth(dateOfBirth);
+            } catch(Exception e) {
+                System.err.println(e.getMessage());
+            }
+            
+            currentPlayer.setUsername(player.getName());
+            //TODO: get real gender
+            currentPlayer.setGender("MALE");
+            
+            players.getPlayer().add(currentPlayer);
+            tournament.setPlayers(players);
+            
+            TournamentType.Rounds rounds = objectFactory.createTournamentTypeRounds();
+            TournamentType.Rounds.Round tournamentRound = objectFactory.createTournamentTypeRoundsRound();
+            tournamentRound.setNumber(0);
+            
+            GameType currentGame = new GameType();
+            try {
+                XMLGregorianCalendar XMLCurrentTime = DatatypeFactory.newInstance().newXMLGregorianCalendar(currentTime);
+                currentGame.setDate(XMLCurrentTime);
+            } catch(Exception e) {
+                System.err.println(e.getMessage());
+            }   
+            currentGame.setStatus("finished");
+            long milliseconds = game.getSpentTime();
+            currentGame.setDuration(BigInteger.valueOf(TimeUnit.MILLISECONDS.toSeconds(milliseconds)));
+            // if leader is computer, send "Computer"
+            if(game.getLeader().getName().equals(game.getComputer().getName())) {
+                currentGame.setWinner("Computer");
+            } else {
+                currentGame.setWinner(game.getLeader().getName());
+            }
+            
+            GameType.Players gamePlayers = objectFactory.createGameTypePlayers();
+            GameType.Players.Player gamePlayer = objectFactory.createGameTypePlayersPlayer();
+            gamePlayer.setRef(game.getPlayer().getName());
+            gamePlayers.getPlayer().add(gamePlayer);
+            currentGame.setPlayers(gamePlayers);
+            
+            tournamentRound.getGame().add(currentGame);
+            rounds.getRound().add(tournamentRound);
+            tournament.setRounds(rounds);
+            highScoreRequest.setTournament(tournament);
+            try {
+                uuid = highScore.publishHighScore(highScoreRequest);
+            } catch(Failure failure) {
+                System.err.println(failure.getFaultInfo().getDetail());
+            }
+            
             Date date = new Date();
-            uuid = "test!?";
 
             TwitterStatusMessage msg = new TwitterStatusMessage(player.getName(), uuid, date);
             try {
